283fb25e74256f2dc53ddda48f47de57ad35ae0f
sassembla <toru.inoue@kissaki.tv>
2021-08-02 14:13:55 +0900

    add UnityPackage for kumamate.

--- Assets/Editor/EditorTool.cs
+++ Assets/Editor/EditorTool.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEditor;
 using System.IO;
+using UnityEngine;
 
 [InitializeOnLoad]
 public class EditorTool
@@ -11,23 +12,41 @@ public class EditorTool
         var assetPaths = new List<string>();
 
         var frameworkPath = "Assets/Kumamate";
-        CollectPathRecursive(frameworkPath, assetPaths);
+        CollectPathRecursive(frameworkPath, assetPaths, new string[] { "Assets/Kumamate/Editor/Storage/", "Assets/Kumamate/Editor/Settings/Fonts/", });
 
         AssetDatabase.ExportPackage(assetPaths.ToArray(), "Kumamate.unitypackage", ExportPackageOptions.IncludeDependencies);
     }
 
-    private static void CollectPathRecursive(string path, List<string> collectedPaths)
+    private static bool ShouldIgnore(string filePath, string[] ignoreFolderPaths)
+    {
+        foreach (var ignoreFolderPath in ignoreFolderPaths)
+        {
+            if (filePath.StartsWith(ignoreFolderPath))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static void CollectPathRecursive(string path, List<string> collectedPaths, string[] ignoreFolderPaths)
     {
         var filePaths = Directory.GetFiles(path);
         foreach (var filePath in filePaths)
         {
+            if (ShouldIgnore(filePath, ignoreFolderPaths))
+            {
+                continue;
+            }
+
             collectedPaths.Add(filePath);
         }
 
         var modulePaths = Directory.GetDirectories(path);
         foreach (var folderPath in modulePaths)
         {
-            CollectPathRecursive(folderPath, collectedPaths);
+            CollectPathRecursive(folderPath, collectedPaths, ignoreFolderPaths);
         }
     }
 }
